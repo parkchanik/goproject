4e383ea21bbca8308506a5f6192c60f53c215308
tivas08 <tivas08@gmail.com>
2018-10-29 18:04:52 +0900

    company2

--- restfullserver.go
+++ restfullserver.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"database/sql"
@@ -11,7 +12,8 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var db, err = sql.Open("mysql", "yprima:Dbtjdwo1$$@tcp(14.63.127.152:10824)/battle_game")
+//var db, err = sql.Open("mysql", "yprima:Dbtjdwo1$$@tcp(14.63.127.152:10824)/battle_game")
+var db, err = sql.Open("mysql", "dbchanik:1q2w3e$r@tcp(localhost:3306)/aboxdb")
  
 
 type User struct {
@@ -25,6 +27,13 @@ type User struct {
 
 }
 
+type SendBox struct {
+	Boxidx 			int 
+	Sender_address 		string 
+	Boxmsg 			string 
+	Send_wei 		int 
+}
+
 
 type Person struct {
 	Id         int
@@ -32,12 +41,12 @@ type Person struct {
 	Last_Name  string
 }
 
-func getById(c *gin.Context) {
+func getMemberInfoByUserNo(c *gin.Context) {
 	var user User 
 
-	id := c.Param("id")
+	user_no := c.Param("user_no")
 	//row := db.QueryRow("select id, username, first_name, middle_name, last_name, email, mobile_phone, login_attempt, active_status FROM MEMBER_INFO where id = ?;", id)
-	row := db.QueryRow("select user_no , server_idx , publisher_id , auth_idx , nickname , gold , input_time FROM MEMBER_INFO where user_no = ?;", id)
+	row := db.QueryRow("select user_no , server_idx , publisher_id , auth_idx , nickname , gold , input_time FROM MEMBER_INFO where user_no = ?;", user_no)
 	
 	err = row.Scan(&user.User_no, &user.Server_idx, &user.Publisher_id, &user.Auth_idx, &user.Nickname, &user.Gold, &user.Input_time)
 	if err != nil {
@@ -47,12 +56,73 @@ func getById(c *gin.Context) {
 	}
 }
 
+func getSendBoxList(c *gin.Context) {
+	var sendbox SendBox 
+
+	//var sendboxs []SendBox
+	//row := db.QueryRow("select id, username, first_name, middle_name, last_name, email, mobile_phone, login_attempt, active_status FROM MEMBER_INFO where id = ?;", id)
+	row := db.QueryRow("CALL SP_SEND_BOX_LIST(1);")
+	/*
+	for rows.Next() {
+		err = rows.Scan(&person.Id, &person.First_Name, &person.Last_Name)
+		persons = append(persons, person)
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+	}
+	defer rows.Close()
+*/
+	err = row.Scan(&sendbox.Boxidx, &sendbox.Sender_address, &sendbox.Boxmsg, &sendbox.Send_wei)
+	if err != nil {
+		c.JSON(http.StatusOK, nil)
+	} else {
+		c.JSON(http.StatusOK, sendbox)
+	}
+}
+
+
+
+func putMemberInfoByUserNo(c *gin.Context) {
+
+	var buffer bytes.Buffer
+	user_no := c.Query("user_no")
+
+	gold := c.PostForm("gold")
+
+	stmt , err := db.Prepare("UPDATE MEMBER_INFO SET gold = ? WHERE user_no = ?;")
+
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+
+	_ , err = stmt.Exec(gold , user_no)
+
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+
+	buffer.WriteString(gold)
+	buffer.WriteString(" ")
+	buffer.WriteString(user_no)
+
+	defer stmt.Close()
+
+	name := buffer.String()
+
+	c.JSON(http.StatusOK , name)
+
+
+
+}
+
 
 func main() {
 	
 
 	router := gin.Default()
-	router.GET("/api/user/:id", getById)
+	router.GET("/api/user/:user_no", getMemberInfoByUserNo)
+
+	router.GET("/sendboxlist", getSendBoxList)
 	
 	router.GET("/person/:id", func(c *gin.Context) {
 		var (
