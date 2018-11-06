6b1adb22194c6565b7dd76cf245377f0586a2924
tivas08 <tivas08@gmail.com>
2018-11-06 17:36:58 +0900

    eventwatch_company_20181106

--- eventwatch.go
+++ eventwatch.go
@@ -6,6 +6,7 @@ import (
     "log"
     "strings"
     "math/big"
+    "database/sql"
 
     "github.com/ethereum/go-ethereum"
     "github.com/ethereum/go-ethereum/accounts/abi"
@@ -16,17 +17,22 @@ import (
     sendabox "./contract" 
 )
 
+//var db, err = sql.Open("mysql", "yprima:Dbtjdwo1$$@tcp(14.63.127.152:10824)/battle_game")
+var db, err = sql.Open("mysql", "dbchanik:1q2w3e$r@tcp(localhost:3306)/aboxdb")
+
 func main() {
-	client, err := ethclient.Dial("wss://ropsten.infura.io/ws")
-	//client, err := ethclient.Dial("ws://localhost:7545")
+	//client, err := ethclient.Dial("wss://ropsten.infura.io/ws")
+	client, err := ethclient.Dial("ws://localhost:8545/ws")
     if err != nil {
 		log.Fatal(err)
 		fmt.Println("err1")
     }
 
-	//contractAddress := common.HexToAddress("0x868eF2CfBd938ca9Ae5ddFa2b6CE2cAdd73b3c36") // local
-	contractAddress := common.HexToAddress("0x8bb30f3e1e5c63f30f3e41cdfaedabe1a45827b9") // ropsten
-	
+    //contractAddress := common.HexToAddress("0x868eF2CfBd938ca9Ae5ddFa2b6CE2cAdd73b3c36") // local
+    contractAddress := common.HexToAddress("0x9e51806f5d074cff7dcea42997860d1176def99b") // local office
+	//contractAddress := common.HexToAddress("0x8bb30f3e1e5c63f30f3e41cdfaedabe1a45827b9") // ropsten
+    
+
     query := ethereum.FilterQuery{
         Addresses: []common.Address{contractAddress},
     }
@@ -51,15 +57,21 @@ func main() {
             fmt.Println("sdd")
 			log.Fatal(err)
         case vLog := <-logs:
+            /*
             fmt.Println("vlog")
             fmt.Println(vLog) // pointer to event log
             fmt.Println(vLog.BlockHash.Hex()) // 0x3404b8c050aa0aacd0223e91b5c32fee6400f357764771d0684fa7b3f448f1a8
             fmt.Println(vLog.BlockNumber)     // 2394201
             fmt.Println(vLog.TxHash.Hex()) 
+            */
+            fmt.Println("blockhash : " + vLog.BlockHash.Hex()) // 0x3404b8c050aa0aacd0223e91b5c32fee6400f357764771d0684fa7b3f448f1a8
+            fmt.Println(vLog.BlockNumber) // 2394201
+            fmt.Println("TxHash : " + vLog.TxHash.Hex())    // 0x280201eda63c9ff6f305fcee51d5eb86167fab40ca3108ec784e8652a0e2b1a6
 
+            
             event := struct {
-                BoxIdx  *big.Int
-                Sender  common.Address
+                //BoxIdx  *big.Int
+                //Sender  common.Address
                 Value   *big.Int
                 Token   *big.Int
                 Message string
@@ -71,10 +83,27 @@ func main() {
                 log.Fatal(err)
             }
             
+            
+        fmt.Printf("Value : %s\n " , event.Value)
+        fmt.Printf("Token : %s\n" , event.Token)
+        fmt.Printf("Message : %s\n" , event.Message)
+        
+        /*
+        var params = [box_idx , sender
+        ,  message , wei ,token
+      , logIndex , transactionIndex , transactionHash , blockHash , blockNumber , id];
+
+      var stmt = 'CALL SP_SEND_BOX(?,?,?,?,?,?,?,?,?,?,?); ';
+        */
+        fmt.Printf("Topics[0] : %s\n " , vLog.Topics[0].Hex()) //topic[0] event Signature
+        fmt.Printf("Topics[1] : %s\n " , vLog.Topics[1].Hex()) //topic[1] 내가 첫번째 index 로 잡았던 event  _box_idx
+        fmt.Printf("Topics[2] : %s\n " , vLog.Topics[2].Hex()) //topic[2] 내가 두번째 index 로 잡았던 event  _sender
+       
+
             fmt.Println("event1")
             fmt.Println(event.BoxIdx)
             fmt.Println("event2")
-            fmt.Println(event.Sender)
+            fmt.Println(event.Sender.Hex())
             fmt.Println(event.Value)
             fmt.Println(event.Token)
             fmt.Println(event.Message)
--- eventwatch_readlog.go
+++ eventwatch_readlog.go
@@ -6,11 +6,14 @@ import (
     "log"
     "strings"
     "math/big"
+    //"strconv"
+   
 
     "github.com/ethereum/go-ethereum"
     "github.com/ethereum/go-ethereum/accounts/abi"
     "github.com/ethereum/go-ethereum/common"
-    "github.com/ethereum/go-ethereum/core/types"
+
+    //"github.com/ethereum/go-ethereum/core/types"
     "github.com/ethereum/go-ethereum/ethclient"
 
     sendabox "./contract" 
@@ -24,11 +27,13 @@ func main() {
 		fmt.Println("err1")
     }
 
-	//contractAddress := common.HexToAddress("0x868eF2CfBd938ca9Ae5ddFa2b6CE2cAdd73b3c36") // local
+    //contractAddress := common.HexToAddress("0x868eF2CfBd938ca9Ae5ddFa2b6CE2cAdd73b3c36") // local house
+    //contractAddress := common.HexToAddress("0x9e51806f5D074Cff7dCea42997860D1176dEF99B") // local office
 	contractAddress := common.HexToAddress("0x8bb30f3e1e5c63f30f3e41cdfaedabe1a45827b9") // ropsten
 	
     query := ethereum.FilterQuery{
-  
+        //FromBlock: big.NewInt(4364359),
+       // ToBlock:   big.NewInt(4364484),
         Addresses: []common.Address{
             contractAddress,
         },
@@ -45,20 +50,24 @@ func main() {
     if err != nil {
         log.Fatal(err)
 	}
-	
+    
+    // go-ethereum/core/types/log.go
 	fmt.Println("3333")
 	for _, vLog := range logs {
-        fmt.Println(vLog.BlockHash.Hex()) // 0x3404b8c050aa0aacd0223e91b5c32fee6400f357764771d0684fa7b3f448f1a8
-        fmt.Println(vLog.BlockNumber)     // 2394201
-        fmt.Println(vLog.TxHash.Hex())    // 0x280201eda63c9ff6f305fcee51d5eb86167fab40ca3108ec784e8652a0e2b1a6
+        fmt.Printf("BlockHash : %s\n" , vLog.BlockHash.Hex()) // 0x3404b8c050aa0aacd0223e91b5c32fee6400f357764771d0684fa7b3f448f1a8
+        fmt.Printf("BlockNumber : %d\n" , vLog.BlockNumber) // 2394201
+        fmt.Printf("TxHash : %s\n" , vLog.TxHash.Hex())    // 0x280201eda63c9ff6f305fcee51d5eb86167fab40ca3108ec784e8652a0e2b1a6
 
+        fmt.Printf("TxIndex  : %d\n" , vLog.TxIndex)    // 
+        fmt.Printf("Index  : %d\n" , vLog.Index)    //  i_logIndex
+         
 		event := struct {
-			BoxIdx  *big.Int
-			Sender  common.Address
+			//BoxIdx  *big.Int
+			//Sender  common.Address
 			Value   *big.Int
 			Token   *big.Int
 			Message string
-			Raw     types.Log // Blockchain specific contextual infos
+			//Raw     types.Log // Blockchain specific contextual infos
 		}{}
 		
         err := contractAbi.Unpack(&event, "ev_SendABoxEvent", vLog.Data)
@@ -66,20 +75,42 @@ func main() {
             log.Fatal(err)
         }
 
-        fmt.Println("event1")
-		fmt.Println(event.BoxIdx)
-		fmt.Println("event2")
-		fmt.Println(event.Sender)
-		fmt.Println(event.Value)
-		fmt.Println(event.Token)
-		fmt.Println(event.Message)
-			
+        //fmt.Println("BoxIdx")
+		//fmt.Println(event.BoxIdx)
+		//fmt.Println("Sender")
+        //fmt.Println(event.Sender.Hex())
+        
+        fmt.Printf("Value : %d\n" , event.Value)
+        fmt.Printf("Token : %d\n" , event.Token)
+        fmt.Printf("Message : %s\n" , event.Message)
+        
+        /*
+        var params = [box_idx , sender
+        ,  message , wei ,token
+      , logIndex , transactionIndex , transactionHash , blockHash , blockNumber , id];
+
+      var stmt = 'CALL SP_SEND_BOX(?,?,?,?,?,?,?,?,?,?,?); ';
+        */
+
+        /*
+        fmt.Printf("Topics[0] : %s\n " , vLog.Topics[0].Hex()) //topic[0] event Signature
+        fmt.Printf("Topics[1] : %s\n " , vLog.Topics[1].Hex()) //topic[1] 내가 첫번째 index 로 잡았던 event  _box_idx
+        fmt.Printf("Topics[2] : %s\n " , vLog.Topics[2].Hex()) //topic[2] 내가 두번째 index 로 잡았던 event  _sender
+        */
+        event_boxidx := vLog.Topics[1].Hex()
+        event_sender := vLog.Topics[2].Hex()
+
+        fmt.Printf("boxid : %d\n" , event_boxidx)
+        fmt.Printf("sender : %s\n" , event_sender)
+        /*
         var topics [4]string
         for i := range vLog.Topics {
             topics[i] = vLog.Topics[i].Hex()
+            fmt.Println(topics[i])
+            
         }
-
-        fmt.Println(topics[0]) // 0xe79e73da417710ae99aa2088575580a60415d359acfad9cdd3382d59c80281d4
+        */  
+        //fmt.Println(topics[0]) // 0xe79e73da417710ae99aa2088575580a60415d359acfad9cdd3382d59c80281d4
 	}
 	
 
